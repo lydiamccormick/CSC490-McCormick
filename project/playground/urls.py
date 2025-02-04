4f6657761023e3b5299c856b566f8d64c3732aaa
lydiamccormick <lydiamccormick22246@gmail.com>
2025-02-04 13:26:00 -0500

    issues

--- project/playground/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-#URLConfiguration
-urlpatterns = [
-    path('hello/', views.say_hello),
-    path('welcome/', views.welcome_message),
-    path('dashboard/', views.dashboard)
-]
--- project/project/urls.py
+++ project/project/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('playground/', include('playground.urls')),
+    path('tasks/', include('tasks.urls')),
+    path('cal/', include('cal.urls')),
 ]
