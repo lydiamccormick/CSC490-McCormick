0370902336bd57c1dda673348931c6b2007c762b
lydiamccormick <lydiamccormick22246@gmail.com>
2025-02-06 13:52:11 -0500

    updates

--- TaskManager/main/main/settings.py
+++ TaskManager/main/main/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "achievements"
-    "cal"
-    "dashboard"
+    "achievements",
+    "cal",
+    "dashboard",
+    "tasks"
+
 ]
 
 MIDDLEWARE = [
---
+++ TaskManager/main/tasks/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from ics import Calendar, Event
+from django.http import HttpResponse
+from .models import Task
+
+def generate_ics(request):
+    calendar = Calendar()
+    
+    for task in Task.objects.all():
+        if task.deadline:
+            event = Event()
+            event.name = task.title
+            event.begin = task.deadline
+            event.description = task.description
+            calendar.events.add(event)
+
+    response = HttpResponse(str(calendar), content_type="text/calendar")
+    response['Content-Disposition'] = 'attachment; filename="tasks.ics"'
+    return response
+
